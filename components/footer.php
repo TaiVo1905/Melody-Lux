881368616b0bab3cc0ca253b4339ebb0a2f68ed3
MaiTram01 <tramh7879@gmail.com>
2024-11-19 08:38:17 +0700

    Done Footer UX/UI

---
+++ components/footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="../public/css/footer.css">
+</head>
+<body>
+    <div class="listen">
+        <div class="media">
+            <div class="media_cd" style="background-image: url('../public/images/singers/chuBin.jpg')"></div>
+            <div class="media_title">
+                <marquee class="media_title--name" scrolldelay="130">Cô đơn trên sofa</marquee>
+                <a class="media_title--author">Chu Bin</a>
+            </div>
+            <div class="media_icon">
+                <li class="meadia_icon_more heart">
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+                </li>
+            </div>
+        </div>
+        <div class="controls">
+            <div class="controls_player">
+                <li class="controls_player--icon"><ion-icon name="shuffle-outline"></ion-icon></i></li>
+                <li class="controls_player--icon"><ion-icon name="play-skip-back-outline"></ion-icon></li>
+                <li class="controls_player--icon big">
+                    <ion-icon name="play-circle-outline"></ion-icon>
+                    <!-- <ion-icon name="pause-outline"></ion-icon> -->
+                </li>
+                <li class="controls_player--icon"><ion-icon name="play-skip-forward-outline"></ion-icon></li>
+                <li class="controls_player--icon"><ion-icon name="repeat-outline"></ion-icon></li>
+            </div>
+            <div class="controls_time">
+                <span class="controls_time--left" style="color:#595560">00.00</span>
+                <input id="progress" class="controls_time--range" type="range" min="0" max="100" value="0" step="1">
+                <span class="controls_time--right">03.22</span>
+            </div>
+        </div>
+        <div class="controls_extend">
+            <div class="controls_extend--lever">
+                <li class="controls_extend--lever_item"><ion-icon name="tv-outline"></ion-icon></li>
+                <li class="controls_extend--lever_item"><ion-icon name="mic-outline"></ion-icon></li>
+                <li class="controls_extend--lever_item"><ion-icon name="volume-high-outline"></ion-icon></li>
+                <!-- <li class="controls_extend--lever_item"><ion-icon name="volume-mute-outline"></ion-icon></li> -->
+                <li class="controls_extend--lever_item_range">
+                    <input id="controls_lever_range" type="range" class="range_volum" value="100" min="0" max="100" step="1">
+                </li>
+                <li class="controls_extend--lever_item"><ion-icon name="musical-note-outline"></ion-icon></li>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
