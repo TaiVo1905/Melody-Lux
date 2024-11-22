06318e9e0742756be5a5894d8dc55e1b7689e738
Vo Duc Tai <voductaitxqt123@gmail.com>
2024-11-22 10:02:10 +0700

    commit to checkout

--- app/components/sidebar.php
+++ app/components/sidebar.php
@@ -1,86 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <link rel="stylesheet" href="../../public/css/sidebar.css?v=<?php echo time(); ?>">
-</head>
-<body>
-    <div class="sidebar">
-        <div class="logo_img">
-            <img src="../public/images/logo/melody_lux.png" alt="">
+<div class="sidebar">
+    <div class="logo_img">
+        <img src="../../public/images/logo/melody_lux.png" alt="">
+    </div>
+    <div class="sidebar_top">
+        <div class="bar_title">
+            <ion-icon class="icon_sidebar" name="home-outline"></ion-icon>
+            <p class="bar_title_content">Thư viện</p>
         </div>
-        <div class="sidebar_top">
-            <div class="bar_title">
-                <ion-icon class="icon_sidebar" name="home-outline"></ion-icon>
-                <p class="bar_title_content">Thư viện</p>
-            </div>
-            <div class="bar_title">
-                <i class="fas fa-bullseye icon_sidebar"></i>
-                <p class="bar_title_content">Khám phá</p>
-            </div>
-            <div class="bar_title">
-                <i class="fas fa-signal icon_sidebar"></i>
-                <p class="bar_title_content">Biểu đồ</p>
-            </div>
-            <div class="bar_title">
-                <ion-icon class="icon_sidebar" name="radio-outline"></ion-icon>
-                <p class="bar_title_content">Radio</p>
-            </div>
+        <div class="bar_title">
+            <i class="fas fa-bullseye icon_sidebar"></i>
+            <p class="bar_title_content">Khám phá</p>
         </div>
-        <div class="sidebar_center">
-            <div class="bar_title">
-                <ion-icon class="icon_sidebar" name="musical-notes-outline"></ion-icon>
-                <p class="bar_title_content">Xếp hạng</p>
-            </div>
-            <div class="bar_title">
-                <ion-icon class="icon_sidebar" name="grid-outline"></ion-icon>
-                <p class="bar_title_content">Thể loại</p>
-            </div>
-            <div class="bar_title">
-                <ion-icon class="icon_sidebar" name="star-outline"></ion-icon>
-                <p class="bar_title_content">Top 100</p>
-            </div>
-            <div class="bar_title bar_title_box">
-                <div class="box_icon_bar icon_time">
-                    <ion-icon class="icon_sidebar" name="timer-outline"></ion-icon>
-                </div>
-                <p class="bar_title_content">Nghe gần đây</p>
-            </div>
-            <div class="bar_title bar_title_box">
-                <div class="box_icon_bar icon_heart">
-                    <ion-icon class="icon_sidebar" name="heart-outline"></ion-icon>
-                </div>
-                <p class="bar_title_content">Bài hát yêu thích</p>
-            </div>
-            <div class="bar_title bar_title_box">
-                <div class="box_icon_bar icon_play">
-                    <ion-icon class="icon_sidebar" name="play-circle-outline"></ion-icon>
-                </div>
-                <p class="bar_title_content">Playlist</p>
-            </div>
-            <div class="bar_title bar_title_box">
-                <div class="box_icon_bar icon_album">
-                    <ion-icon class="icon_sidebar" name="albums-outline"></ion-icon>
-                </div>
-                <p class="bar_title_content">Album</p>
-            </div>
-            <div class="bar_title bar_title_box">
-                <div class="box_icon_bar icon_upload">
-                    <ion-icon class="icon_sidebar" name="cloud-upload-outline"></ion-icon>
-                </div>
-                <p class="bar_title_content">Tải lên</p>
-            </div>
+        <div class="bar_title">
+            <i class="fas fa-signal icon_sidebar"></i>
+            <p class="bar_title_content">Biểu đồ</p>
         </div>
-        <div class="add_playlist">
-            <ion-icon class="icon_sidebar" name="add-outline"></ion-icon>
-            <p class="bar_title_content">Tạo playlist mới</p>
+        <div class="bar_title">
+            <ion-icon class="icon_sidebar" name="radio-outline"></ion-icon>
+            <p class="bar_title_content">Radio</p>
         </div>
     </div>
-</body>
-</html>
+    <div class="sidebar_center">
+        <div class="bar_title">
+            <ion-icon class="icon_sidebar" name="musical-notes-outline"></ion-icon>
+            <p class="bar_title_content">Xếp hạng</p>
+        </div>
+        <div class="bar_title">
+            <ion-icon class="icon_sidebar" name="grid-outline"></ion-icon>
+            <p class="bar_title_content">Thể loại</p>
+        </div>
+        <div class="bar_title">
+            <ion-icon class="icon_sidebar" name="star-outline"></ion-icon>
+            <p class="bar_title_content">Top 100</p>
+        </div>
+        <div class="bar_title bar_title_box">
+            <div class="box_icon_bar icon_time">
+                <ion-icon class="icon_sidebar" name="timer-outline"></ion-icon>
+            </div>
+            <p class="bar_title_content">Nghe gần đây</p>
+        </div>
+        <div class="bar_title bar_title_box">
+            <div class="box_icon_bar icon_heart">
+                <ion-icon class="icon_sidebar" name="heart-outline"></ion-icon>
+            </div>
+            <p class="bar_title_content">Bài hát yêu thích</p>
+        </div>
+        <div class="bar_title bar_title_box">
+            <div class="box_icon_bar icon_play">
+                <ion-icon class="icon_sidebar" name="play-circle-outline"></ion-icon>
+            </div>
+            <p class="bar_title_content">Playlist</p>
+        </div>
+        <div class="bar_title bar_title_box">
+            <div class="box_icon_bar icon_album">
+                <ion-icon class="icon_sidebar" name="albums-outline"></ion-icon>
+            </div>
+            <p class="bar_title_content">Album</p>
+        </div>
+        <div class="bar_title bar_title_box">
+            <div class="box_icon_bar icon_upload">
+                <ion-icon class="icon_sidebar" name="cloud-upload-outline"></ion-icon>
+            </div>
+            <p class="bar_title_content">Tải lên</p>
+        </div>
+    </div>
+    <div class="add_playlist">
+        <ion-icon class="icon_sidebar" name="add-outline"></ion-icon>
+        <p class="bar_title_content">Tạo playlist mới</p>
+    </div>
+</div>
---
+++ app/views/libraryView.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thư viện</title>
+    <?php include_once("../components/linkbootstrap.php");?>
+    <link href="../../public/css/grid.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="../../public/css/sidebar.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../../public/css/song-item.css?v=<?php echo time(); ?>">
+    <style>
+        .right {
+            background-color: #061d4f;
+    margin-top: 70px;
+
+        }
+        h1 {
+            color: #fff;
+            padding-left: 12px;
+            padding-top: 12px;
+        }
+        .nav_label--sidebar_croller {
+    width: 100%;
+    height: 100vh;
+    overflow-y: auto;
+    cursor: default;
+    /* margin-top: 6px; */
+    padding-top: 6px;
+}
+.nav_label--sidebar_croller::-webkit-scrollbar{
+    height: 0px;
+    width: 0px;
+}
+
+.nav_label--sidebar_croller.active::-webkit-scrollbar{
+    height:4px;
+    width:4px;
+}
+
+.nav_label--sidebar_croller::-webkit-scrollbar-track{
+    /* --webkit-box-shadow: inset 0 0 0 #ffffff; */
+    /* background: #231B2E; */     /* background: của thanh*/
+    border:0 solid #000
+}
+
+/* thanh trượt bên trong */
+.nav_label--sidebar_croller::-webkit-scrollbar-thumb
+{
+    --webkit-box-shadow:inset 0 0 0 #ffffff;
+    background:#82878a; 
+    border-radius:4px 4px 4px 4px;
+}
+.container_profile--head_list{
+    width: 730px;
+    height: 32px;
+    background-color: #1f3461r;
+    display: flex;
+    border-radius: 16px;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.container_profile--head_list--item{
+    width: 100px;
+    height: 28px;
+    list-style: none;
+    line-height: 28px;
+    text-align: center;
+    border-radius: 16px;
+    cursor: pointer;
+    /* color: #f6f6db; */
+    color: #c2d5d6;
+    text-transform: uppercase;
+    font-size: 12px;
+}
+
+.container_profile--head_list--item:hover{
+    /* color: #9a59e4; */
+    color: #fff;
+}
+
+.container_profile--head_list--item:nth-child(1){
+    width: 124px;
+}
+
+.container_profile--head_list--item.active{
+    background-color: #6e6875;
+    color: #fff;;
+}
+
+.container_profile--layout__sum--foot_img{
+    width: 270px;
+    height: 100%;
+    /* background-color: #5a4be7; */
+    background-size: cover;
+    position: relative;
+}
+
+.container_profile--layout__sum--foot_img-item{
+    width: 100%;
+    height: 100%;
+    list-style: none;
+    z-index: -1;
+    background-size: cover;
+    border-radius: 4px;
+    display: none;
+}
+
+.container_profile--layout__sum--foot_img-item.second,
+.container_profile--layout__sum--foot_img-item.third,
+.container_profile--layout__sum--foot_img-item.first{
+    display: block;
+}
+
+.first{
+    width: 230px;
+    height: 230px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 3;
+    transition: 1s linear 0.02s;
+}
+
+.second{
+    width: 196px;
+    height: 196px;
+    position: absolute;
+    top: calc((230px - 196px) / 2);
+    left: 16px;
+    z-index: 2;
+    opacity: 0.7;
+    transition: 1s linear 0.02s;
+}
+
+.third{
+    width: 162px;
+    height: 162px;
+    position: absolute;
+    top: calc((230px - 162px) / 2);
+    left: 0;
+    z-index: 1;
+    margin: auto;
+    opacity: 0.3;
+    transition: 1s linear 0.02s;
+}
+.songs{
+    height: 244px;
+    /* margin-left: 20px; */
+    overflow-y: scroll;
+}
+
+
+    </style>
+    
+</head>
+<body>
+    <div class="grid">
+        <div class="row no-gutters">
+            <div class="sidebar col c-3 nav_label--sidebar_croller">
+                <?php include_once("../components/sidebar.php");
+                ?>
+            </div>
+            <div class="right col c-9">
+                <div class="header">
+                    <?php include_once("../components/header.php");?>
+                </div>
+                <h1>Thư viện</h1>
+                <div class="container_profile--head_list">
+                    <li class="container_profile--head_list--item active">TỔNG QUAN</li>
+                    <li class="container_profile--head_list--item">Bài hát</li>
+                    <li class="container_profile--head_list--item">PLAYLIST</li>
+                    <li class="container_profile--head_list--item hion-on-mobile">Nghệ sĩ</li>
+                    <li class="container_profile--head_list--item">ALBUM</li>
+                    <li class="container_profile--head_list--item hion-on-mobile">MV</li>
+                    <li class="container_profile--head_list--item hion-on-mobile">Tải Lên</li>
+                </div>
+                <div class="list-song row">
+                <h1>Bài hát</h1>
+
+                    <div class="col col-3">
+                    <div class="container_profile--layout__sum--foot_img hion-on-mobile">
+                                    <li class="container_profile--layout__sum--foot_img-item second hion-on-mobile" style="background-image: url(../../public/images/singers/chuBin.jpg)">
+
+                                    </li>
+                                    <li class="container_profile--layout__sum--foot_img-item third hion-on-mobile" style="background-image: url(../../public/images/singers/chuBin.jpg)">
+                                        
+                                    </li>
+                                    <li class="container_profile--layout__sum--foot_img-item first hion-on-mobile" style="background-image: url(../../public/images/singers/chuBin.jpg)">
+                                        
+                                    </li>
+                                </div>
+                    </div>
+                    <div class="songs col col-9">
+                        <?php include_once("../components/song-item.php");
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                echo createSong();
+                        
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    <script>
+        const profile_head_list = document.querySelectorAll('.container_profile--head_list--item');
+
+
+        profile_head_list.forEach((item, index) => {
+            item.onclick = function (){
+                document.querySelector('.container_profile--head_list--item.active').classList.remove('active');
+                this.classList.add('active');
+            }
+        })
+
+
+        const img_animate_first = document.querySelectorAll('.container_profile--layout__sum--foot_img-item')[0];
+    const img_animate_second = document.querySelectorAll('.container_profile--layout__sum--foot_img-item')[1];
+    const img_animate_third = document.querySelectorAll('.container_profile--layout__sum--foot_img-item')[2];
+    setTimeout(function(){
+        setTimeout(function(){
+            img_animate_first.classList.remove('second')
+            img_animate_second.classList.remove('third')
+            img_animate_third.classList.remove('first')
+
+            img_animate_first.classList.add('first')
+            img_animate_second.classList.add('second')
+            img_animate_third.classList.add('third')
+        },1000)
+        setTimeout(function(){
+            img_animate_first.classList.remove('first')
+            img_animate_second.classList.remove('second')
+            img_animate_third.classList.remove('third')
+
+            img_animate_first.classList.add('third')
+            img_animate_second.classList.add('first')
+            img_animate_third.classList.add('second')
+        },3000)
+        setTimeout(function(){
+            img_animate_first.classList.remove('third')
+            img_animate_second.classList.remove('first')
+            img_animate_third.classList.remove('second')
+            
+            img_animate_first.classList.add('second')
+            img_animate_second.classList.add('third')
+            img_animate_third.classList.add('first')
+        },5000)
+    },0)
+    setInterval(function(){
+        setTimeout(function(){
+            img_animate_first.classList.remove('second')
+            img_animate_second.classList.remove('third')
+            img_animate_third.classList.remove('first')
+
+            img_animate_first.classList.add('first')
+            img_animate_second.classList.add('second')
+            img_animate_third.classList.add('third')
+        },1000)
+        setTimeout(function(){
+            img_animate_first.classList.remove('first')
+            img_animate_second.classList.remove('second')
+            img_animate_third.classList.remove('third')
+
+            img_animate_first.classList.add('third')
+            img_animate_second.classList.add('first')
+            img_animate_third.classList.add('second')
+        },3000)
+        setTimeout(function(){
+            img_animate_first.classList.remove('third')
+            img_animate_second.classList.remove('first')
+            img_animate_third.classList.remove('second')
+            
+            img_animate_first.classList.add('second')
+            img_animate_second.classList.add('third')
+            img_animate_third.classList.add('first')
+        },5000)
+    },6000)
+    </script>
+</html>
